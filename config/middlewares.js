e2896c80111cf6652ae83750f1cec9d58ee6e919
lichin <lichin@lichindeMacBook-Pro.local>
2025-10-28 21:02:27 +0400

    feat: add tiny-cme plugins

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,37 +1,7 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,  
-        directives: {
-          "script-src": ["'self'", "*.tinymce.com", "*.tiny.cloud", "https:"],
-          "connect-src": ["'self'", "*.tinymce.com", "*.tiny.cloud", "blob:", "*.strapi.io"],
-          "img-src": [
-            "'self'",
-            "*.tinymce.com",
-            "*.tiny.cloud",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            "strapi.io",
-            "s3.amazonaws.com",
-            "cdn.jsdelivr.net",
-          ],
-          "style-src": [
-            "'self'",
-            "'unsafe-inline'",
-            "*.tinymce.com",
-            "*.tiny.cloud",
-          ],
-          "font-src": ["'self'", "*.tinymce.com", "*.tiny.cloud"],
-        },
-        upgradeInsecureRequests: null,
-      },
-    },
-  },
+  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,2 @@
 module.exports = () => ({
-  tinymce:{
-    enabled:true
-  }
 });
